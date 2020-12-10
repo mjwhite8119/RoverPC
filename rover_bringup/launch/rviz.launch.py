4e7d9dee248921154e752c10eb392cfd9e625c1f
Martin White <11601920+mjwhite8119@users.noreply.github.com>
2020-12-10 15:03:14 -0800

    first commit

---
+++ rover_bringup/launch/rviz.launch.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Author: Martin White
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    rviz_config_dir = os.path.join(
+        get_package_share_directory('rover_description'),
+        'rviz',
+        'model.rviz')
+
+    return LaunchDescription([
+        Node(
+            package='rviz2',
+            node_executable='rviz2',
+            node_name='rviz2',
+            arguments=['-d', rviz_config_dir],
+            output='screen'),
+    ])
